f3eb32b0509693a0bdff5c59c2f0050da2dfc4e7
yuming0315 <gjdbal111@naver.com>
2023-01-06 15:10:47 +0900

    first commit

---
+++ sql연습/basic-query.sql
@@ -0,0 +1,37 @@
+select version(),current_date,now()
+from dual;
+-- 주석 사칙연산 가능
+select sin(pi() / 4),1+2+3+4/5 as 사칙연산 from dual;
+
+-- 대소문자 구분 안함, 칼럼이름도 대소문자 구분안함!
+select version(), CURRENT_DATE, NOW() FROM DUAL;
+
+show databases;
+
+use test;
+show tables;
+
+-- table 생성: DML
+create table pet (
+	name varchar(100),
+	owner varchar(20),
+	species varchar(20),
+	gender char(1),
+	birth date,
+	death date
+);
+
+-- schema 확인
+desc pet;
+describe pet;
+
+-- table 삭제
+drop table pet;
+show tables;
+
+-- insert: DML(C)
+insert into pet values('성탄이','안대혁','dog','m','2007-12-25',null);
+
+select * from pet;
+
+
