b9ee0ac19bdaaa94cd823181e25a761f01b558ec
yuming0315 <gjdbal111@naver.com>
2023-01-09 11:34:33 +0900

    basic sql #2

---
+++ sql
@@ -0,0 +1,8 @@
+-- select 연습
+select * from departments;
+select count(*) from departments;
+
+select dept_no,dept_name from departments;
+
+-- as (alias, 생략 가능)
+
--- sql연습/basic-query.sql
+++ sql연습/basic-query.sql
@@ -48,6 +48,60 @@ load data local infile 'd:\pet.txt' into table pet;
 -- select
 select * from pet where name = 'Bowser';
 
+update pet set death = null where name != 'Bowser';
+
+select name, species, gender
+from pet
+where species = 'dog' and gender = 'f';
+
+
+select name, species, gender
+from pet
+where species in('snake','bird');
+
+-- 아리스타처럼 꼭 명시해야하는 것은
+-- orderby는 필수 . 데이터를 삭제하고 입력하고를 반복하다보면 순서가 꼬임 ( 2번자리에 5번째 저장됨)
+
+select name, birth
+from pet
+order by birth desc;
+
+select name, birth,death
+from pet
+where death is not null;
+
+select name
+from pet
+where name like 'b%';
+
+select name
+from pet
+where name like '%fy';
+
+
+select name
+from pet
+where name like '%w%';
+-- %는 문자수에 제한 x  _는 한문자
+
+
+select name
+from pet
+where name like '_____';
+-- b로 시작하는 5글자 'b____'
+
+select count(*)
+from pet;
+
+-- 널이 아닌 칼럼의 수
+select count(death)
+from pet;
+-- 위의 문장보다 아래처럼
+select count(*)
+from pet
+where death is null;
+
+
 
 
 
