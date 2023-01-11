4937862b36030afab04d5f26043feecb3ff3c051
yuming0315 <gjdbal111@naver.com>
2023-01-11 11:13:07 +0900

    subquery #2

--- sql연습/subquery.sql
+++ sql연습/subquery.sql
@@ -68,6 +68,78 @@ group by a.title
 ) a
 );
 
--- 3-2) 복수행 연산자 
+-- 3-2) 복수행 연산자 in, not in, 비교연산자 any, 비교연산자 all
+
+-- any 사용법
+-- 1. =any: in
+-- 2. >any, >=any: 최소값
+-- 3. <any, <=any: 최대값
+-- 4. <>any, !=any: not in
+
+-- all 사용법
+-- 1. =all: (x)
+-- 2. >all, >=all: 최대값
+-- 3. <all, <=all: 최소값
+-- 4. <>all, !=all
+
+-- 실습문제3
+-- 현재 급여가 50000 이상인 직원의 이름과 급여를 출력하세요.
+-- 둘리 6000
+-- 또치 8000
+
+-- sol1)
+select a.first_name, b.salary
+from employees a join salaries b on a.emp_no = b.emp_no
+where b.to_date >= sysdate() and b.salary > 50000
+order by b.salary;
+
+-- sol2) subquery
+select emp_no, salary
+from salaries
+where to_date >= sysdate() and salary > 50000;
+
+select a.first_name, b.salary
+from employees a join salaries b on a.emp_no = b.emp_no
+where b.to_date >= sysdate() and (a.emp_no, b.salary) in   -- any도 가능
+(
+select emp_no, salary
+from salaries
+where to_date >= sysdate() and salary > 50000
+)order by b.salary;
+
+-- 실습문제4: 현재, 각 부서별로 최고 급여를 받고 있는 직원의 이름과 월급을 출력하세요
+-- 총무 둘리 4000
+-- 영업 또치 5000
+
+-- sol1: where절 subquery (in)
+select a.dept_no,max(b.salary)
+from dept_emp a join salaries b on a.emp_no = b.emp_no
+where a.to_date >= sysdate() and b.to_date >= sysdate()
+group by a.dept_no;
+
+-- sol2: from절 subquery & join
+select a.dept_name, c.first_name, d.salary
+from departments a join dept_emp b on a.dept_no = b.dept_no
+join employees c on b.emp_no = c.emp_no
+join salaries d on c.emp_no = d.emp_no
+where b.to_date >= sysdate() and d.to_date >= sysdate() and (a.dept_no,d.salary) in 
+(
+select a.dept_no,max(b.salary)
+from dept_emp a join salaries b on a.emp_no = b.emp_no
+where a.to_date >= sysdate() and b.to_date >= sysdate()
+group by a.dept_no
+);
+
+select a.dept_name, c.first_name, d.salary
+from departments a join dept_emp b on a.dept_no = b.dept_no
+join employees c on b.emp_no = c.emp_no
+join salaries d on c.emp_no = d.emp_no
+join 
+(select a.dept_no 부서,max(b.salary) 최대연봉
+from dept_emp a join salaries b on a.emp_no = b.emp_no
+where a.to_date >= sysdate() and b.to_date >= sysdate()
+group by a.dept_no) e on a.dept_no = e.부서 and d.salary = e.최대연봉
+where b.to_date >= sysdate() and d.to_date >= sysdate();
+
 
 
